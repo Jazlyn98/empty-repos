86e2edad923d822a912e24707f31af01264e4d23
Jazlyn98 <tranhongcam52@gmail.com>
2025-11-08 22:44:53 +0700

    Done_TC1

--- src/test_web/test_case_1.py
+++ src/test_web/test_case_1.py
@@ -8,43 +8,74 @@ from selenium.webdriver.support.ui import Select
 chrome_option= webdriver.ChromeOptions()
 driver= webdriver.Chrome(chrome_option)
 
-#test case 1: verify login function
-
-sleep(1)
-driver.get ("https://www.staging.littlelives.com/signin")
+driver.get ("https://moveek.com/")
 sleep(3)
 print (f"main_title:{driver.title}")
 sleep(3)
 
-#kiem tra nut sign in disable
-signin_button= driver.find_element (By.ID,"btn-submit-sign-in")
-is_disabled = not signin_button.is_enabled() 
-if is_disabled:
-    print("Nút 'Login' bị vô hiệu hóa khi không nhập email và password.")
+#Click on the “Rạp” dropdown arrow
+dropdown_cinames= driver.find_element(By.XPATH,"//a[@href='/rap/']")
+dropdown_cinames.click()
+sleep(5)
+modal = driver.find_element(By.XPATH, "//div[@id='cinemaModal']//div[@class='modal-content']")
+if modal.is_displayed():
+    print("Modal is displayed")
 else:
-    print("Nút 'Login' không bị vô hiệu hóa khi không nhập email và password.")
+    raise Exception("Modal not visible")
+
+#In the search box, type “Đà Nẵng”
+
+combobox= driver.find_element (By.XPATH,"//span[@role='combobox']")
+combobox.click()
+sleep(2)
+Search_box= driver.find_element (By.XPATH,"//span[1]/input[@type='search']")
+Search_box.send_keys("Đà Nẵng",Keys.ENTER)
 sleep(3)
 
-email= driver.find_element (By.XPATH,"/html/body/div[1]/div/div/main/div/div/div/div/div/form/div[1]/div/input")
-email.send_keys ("sunny.trainc_w81017_admin@mailinator.com")
-sleep(3)
-password= driver.find_element (By.XPATH,"/html/body/div[1]/div/div/main/div/div/div/div/div/form/div[2]/div/input")
-password.send_keys ("Traninc@456")
-sleep(3)
-#kiem tra nut sign in enable
-is_enabled = signin_button.is_enabled()
-if is_enabled:
-    print("Nút 'Login' đã được kích hoạt khi nhập email và password.")
-else:
-    print("Nút 'Login' chưa được kích hoạt dù đã nhập email và password.")
+#All cinemas in Đà Nẵng are displayed
+cinema_list = driver.find_elements(By.CSS_SELECTOR, "[data-name]")
+cinema_names = []  # create an empty list to store theater names
+for c in cinema_list:
+    name = c.get_attribute("data-name") # get the name attribute
+    if name:
+        cinema_names.append(name)   # add name to the list
+print("theater names")
+for name in cinema_names:
+    print("-", name)
+
+#Select “Metiz Cinema” from the list
+metiz_cinema= driver.find_elements(By.XPATH,"//h4/a[contains(text(), 'Metiz Cinema')]")
+for cinema in metiz_cinema:
+    cinema.click()
+    break
 sleep(5)
-# verify login success
-signin_button.click ()
-sleep(5)
-expected_title ="Dashboard"
-assert expected_title in driver.title, "Đăng nhập thất bại, tiêu đề không khớp"
-print ("SUCCESS")
-#test
-ori_windown= driver.current_window_handle
-assert len (driver.window_handles) ==1, print (f"assert isWindowsNumberPassed FAILED")
-print ("PASSED")
+
+#Print the selected date
+selected_date=driver.find_elements(By.XPATH,"//*[@id='dates']/a[1]")
+for date in selected_date:
+    date_str= date.get_attribute("data-date")
+    weekday= date.find_element(By.XPATH,"//*[@id='dates']/a[1]/span").text.strip()
+    print(f"selected date: {date_str},{weekday}")
+    break
+sleep(2)
+
+#Get the name of the first movie in the list
+first_movie= driver.find_elements(By.XPATH,"//div[2]/div/div/div[2]/h4[@class='card-title mb-1 name']/a") #ko bk rút gọn hơn
+for movie in first_movie:
+    movie_name= movie.text
+    print (f"first movie name:{movie_name}")
+    break
+sleep(2)
+
+#Select the earliest available time
+# locate the firsr movie
+time_container = movie.find_element(By.XPATH, "//*[@id='showtimes']/div/div[2]/div/div/div[2]/div/div") #ko bk rút gọn dong nay
+# Get all time
+time_slots = time_container.find_elements(By.XPATH, ".//span[contains(@class,'time')]")
+
+if time_slots:
+    earliest_time = time_slots[0].text.strip()  # get the text of the first time
+    print(f"Earliest showtime: {earliest_time}")  
+    time_slots[0].click()  
+sleep(3)
+  
