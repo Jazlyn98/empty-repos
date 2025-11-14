577c8d3148348e341841c9540cb45aa981683dcc
yun <hugh@enclave.vn>
2025-11-15 01:03:05 +0700

    can run all steps

--- src/test_web/test_case_1.py
+++ src/test_web/test_case_1.py
@@ -15,22 +15,28 @@ print (f"main_title:{driver.title}")
 sleep(10)
 
 #Click on the “Rạp” dropdown arrow
-dropdown_cinames= driver.find_element(By.XPATH,'//*[@id="navbar"]//a[@href="/rap/"]')
-dropdown_cinames.click()
+dropdown_cinemas= driver.find_element(By.XPATH,'//*[@id="navbar"]//a[@href="/rap/"]')
+dropdown_cinemas.click()
 sleep(5)
-modal = driver.find_element(By.XPATH, "//div[@id='cinemaModal']//div[@class='modal-content']")
+modal = driver.find_element(By.XPATH, '//div[@id="cinemaModal"]//div[@class="modal-content"]')
 if modal.is_displayed():
     print("Modal is displayed")
 else:
     raise Exception("Modal not visible")
 
-#In the search box, type “Đà Nẵng”
+actions = ActionChains(driver) #use ActionChains to open Select box/Combobox to select item
 
-combobox= driver.find_element (By.XPATH,"//span[@role='combobox']")
-combobox.click()
-sleep(2)
-Search_box= driver.find_element (By.XPATH,"//span[1]/input[@type='search']")
-Search_box.send_keys("Đà Nẵng",Keys.ENTER)
+select_box = modal.find_element(By.XPATH,'//select')
+actions.click(select_box).perform()
+
+child_elements = select_box.find_elements(By.XPATH, ".//*") 
+
+# You can iterate through the child_elements list
+for child in child_elements:
+    if child.text == "Đà Nẵng":
+        print(f"3333333 city: {child.text}")
+        child.click()
+
 sleep(3)
 
 #All cinemas in Đà Nẵng are displayed
@@ -45,55 +51,51 @@ for name in cinema_names:
     print("-", name)
 
 #Select “Metiz Cinema” from the list
-metiz_cinema= driver.find_elements(By.XPATH,"//h4/a[contains(text(), 'Metiz Cinema')]")
-for cinema in metiz_cinema:
-    cinema.click()
-    break
+metiz_cinema= driver.find_element(By.XPATH,"//h4/a[contains(text(), 'Metiz Cinema')]")
+metiz_cinema.click()
 sleep(5)
 
+# the parent element of date and list of movies
+showtimes_element = driver.find_element(By.XPATH,'//*[@id="showtimes"]/div')
+
 #Print the selected date
-selected_date=driver.find_elements(By.XPATH,"//*[@id='dates']/a[1]")
-for date in selected_date:
-    date_str= date.get_attribute("data-date")
-    weekday= date.find_element(By.XPATH,"//*[@id='dates']/a[1]/span").text.strip()
-    print(f"selected date: {date_str},{weekday}")
-    break
+selected_date=showtimes_element.find_element(By.XPATH,'.//div[@id="dates"]/a[contains(@class, "active")]') #the selected date will have class "active"
+date_str= selected_date.get_attribute("data-date")
+weekday= selected_date.find_element(By.XPATH,".//span").text.strip()
+print(f"selected date: {date_str},{weekday}")
 sleep(2)
 
 #Get the name of the first movie in the list
-first_movie= driver.find_elements(By.XPATH,'//*[@id="app"]//h4[@class="card-title mb-1 name"]/a')
-for movie in first_movie:
-    movie_name= movie.text
-    print (f"first movie name:{movie_name}")
-    break
+first_movie_today= showtimes_element.find_element(By.XPATH,'./div[3]') # ignore div[1] - the "dates" and the div[2] - "sponsor movie"
+print (f"11111 first_movie_today:{first_movie_today.text}")
 sleep(2)
 
 #Select the earliest available time
-time_container = movie.find_element(By.XPATH, "//*[@id='showtimes']//div[@class='mb-1']") # locate the firsr movie
-time_list = time_container.find_elements(By.XPATH, ".//span[contains(@class,'time')]") # Get all time
-
-if time_list:
-    earliest_time = time_list[0].text.strip()  # get the text of the first time
-    print(f"Earliest showtime: {earliest_time}")
-    earliest_time_link = time_list[0].find_element(By.XPATH, './parent::a') # need to get parent element
+time_list_today = first_movie_today.find_elements(By.XPATH, './/a[contains(@class, "showtime")]') # locate the showtimes of first movie
+earliest_time_today = time_list_today[0] # get the first time in list
+print(f"Earliest showtime today: {earliest_time_today.text.strip()}")
+try:
+    earliest_time_today.click()
+    print("Clicked the earliest showtime today.")
+except Exception:
+    print("Earliest showtime today has passed — cannot click")
+    print("try tomorrow")
+    #get the earliest time of the next day (tomorrow)
+    tomorrow = showtimes_element.find_elements(By.XPATH,'.//div[@id="dates"]/a[2]') # a[1] is the first date in list, a[2] is tomorrow
+    tomorrow.click()
+    sleep(3)
+    #Get back the showtime list after moving to tomorrow
+    first_movie_tomorrow= showtimes_element.find_element(By.XPATH,'.//div[3]')
+    # get first movie again, because element change and tomorrow movies list may change
+    time_list_tomorrow = first_movie_tomorrow.find_element(By.XPATH, './/a[contains(@class, "showtime")]') # locate the first showtime of first movie
+    earliest_time_tomorrow = time_list_tomorrow[0]
+    print(f"Earliest showtime tomorrow: {earliest_time_tomorrow.text.strip()}")
     try:
-     earliest_time_link.click()
-     print("Clicked the earliest showtime today.")
+        earliest_time_tomorrow.click()
+        print("Clicked the earliest showtime tomorrow.")
     except Exception:
-        print("Earliest showtime today has passed — cannot click")
+        print("Error — cannot click")
+    sleep(3)
 sleep(3)
 
-#get the earliest time of the next day (tomorrow)
-tomorrow = driver.find_element(By.XPATH,'//*[@id="dates"]/a[2]')
-tomorrow.click()
-sleep(3)
-#Get back the showtime list after moving to tomorrow
-time_container = movie.find_element(By.XPATH, "//*[@id='showtimes']//div[@class='mb-1']")
-time_list = time_container.find_elements(By.XPATH, ".//span[contains(@class,'time')]")
-if time_list:
-    earliest_time_link = time_list[0].find_element(By.XPATH, './parent::a') 
-    earliest_time_link.click()
-    print("Clicked the earliest showtime tomorrow")
-sleep(3)
-
-    
+driver.quit() # always have this to quit the test correctly
